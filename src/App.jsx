f3ccf7f55e90cfea93c63b1550aa61ce9e3aa996
Lais Santos <lais.santos.cris05@gmail.com>
2025-11-17 11:47:14 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,190 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
+import style from "./App.module.css";
+import foto from "/foto.png"
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [dados, setDados] = useState([]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <nav className={style.menu}>
+        <a href="#s1">Home</a>
+        <a href="#s2">𝐕𝐢𝐝𝐚 𝐞 𝐋𝐢𝐛𝐞𝐫𝐝𝐚𝐝𝐞</a>
+        <a href="#s3">𝐈𝐠𝐮𝐚𝐥𝐝𝐚𝐝𝐞 𝐞 𝐒𝐞𝐠𝐮𝐫𝐚𝐧ç𝐚</a>
+        <a href="#s4">𝐋𝐢𝐛𝐞𝐫𝐝𝐚𝐝𝐞 𝐝𝐞 𝐞𝐱𝐩𝐫𝐞𝐬𝐬ã𝐨</a>
+        <a href="#s5">𝐓𝐫𝐚𝐛𝐚𝐥𝐡𝐨 𝐞 𝐄𝐝𝐮𝐜𝐚çã𝐨</a>
+      </nav>
+
+      <section className={style.s1} id="s1">
+        <h2 className={style.tecTitle}>Direitos Humanos</h2>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          São garantias fundamentais inerentes a todos os seres humanos, que
+          visam assegurar que vivam com dignidade, liberdade, igualdade e
+          justiça.Esses diretos vem do artigo 5 da Constituição Federal de 1988.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </section>
+
+      <section className={style.s2} id="s2">
+        <div>
+          <h2>Direito a Vida</h2>
+          <p>
+            O direito à vida é o direito fundamental de um indivíduo existir e
+            ter uma vida digna, protegido pela Constituição Federal brasileira.
+            Ele abrange não apenas a sobrevivência física, mas também a
+            integridade física e moral, o direito de nascer e de ter acesso a
+            condições mínimas para uma existência digna, como alimentação,
+            moradia e saúde.
+            <br />
+            POR QUE FOI CRIADO o direito à vida foi "criado" (ou reconhecido e
+            formalizado legalmente) para estabelecer a base da convivência
+            humana, garantindo a existência física e uma vida digna a todos os
+            indivíduos, protegendo-os de ações arbitrárias, sejam elas de outros
+            indivíduos ou do próprio Estado.
+          </p>
+        </div>
+        <div>
+          <h2>Direito à Liberdade</h2>
+          <p>
+            "À liberdade" pode se referir a um conceito filosófico e jurídico
+            amplo, que engloba a autonomia individual e a ausência de coação. É
+            também um direito fundamental em diversas constituições, que garante
+            a liberdade de expressão, de locomoção, de pensamento, de crença e
+            de reunião, entre outras. A liberdade é limitada apenas pela
+            necessidade de não infringir os direitos de outras pessoas ou o
+            direito de propriedade.
+            <br />
+            Por que foi criado? O direito à liberdade foi criado principalmente
+            como uma resposta aos horrores da Segunda Guerra Mundial, com o
+            objetivo de proteger a dignidade humana e estabelecer um consenso
+            universal sobre o valor inalienável da vida e da liberdade de todos
+            os indivíduos. A Declaração Universal dos Direitos Humanos, adotada
+            em 1948 pela ONU, foi um marco fundamental que consagrou a liberdade
+            como um direito fundamental e um pilar para a paz e a justiça. A
+            criação desse direito também visa limitar o poder do Estado sobre o
+            indivíduo, garantindo que as pessoas possam viver, se expressar e
+            agir livremente, desde que respeitem os direitos dos outros.
+          </p>
+        </div>
+      </section>
+
+      <section className={style.s3} id="s3">
+        <div>
+          <h2>igualdade</h2>
+          <p>
+            "À igualdade" refere-se ao princípio de tratar todos de forma igual
+            perante a lei e a sociedade, garantindo os mesmos direitos e
+            deveres. No entanto, para que essa igualdade se concretize, o
+            tratamento não pode ser sempre o mesmo; é necessário considerar as
+            desigualdades existentes para garantir a igualdade material. Isso
+            significa tratar "os iguais de forma igual e os desiguais de forma
+            desigual, na medida de sua desigualdade", como afirmado por Rui
+            Barbosa e previsto na Constituição, exigindo ações que promovam a
+            equidade, como as ações afirmativas.
+            <br />O conceito de igualdade foi desenvolvido e formalizado para
+            estabelecer a justiça social, garantir que todos os seres humanos
+            tenham o mesmo status social e oportunidades, e combater privilégios
+            e opressões baseados em diferenças infundadas.
+          </p>
+        </div>
+
+        <div>
+          <h2>Segurança pessoal</h2>
+          <p>
+            A segurança pessoal é um conjunto de medidas preventivas e reativas
+            para proteger a integridade física, mental e moral de um indivíduo,
+            incluindo o seu patrimônio. Ela pode ser realizada tanto por meio de
+            ações individuais, como estar atento ao ambiente e evitar locais
+            perigosos, quanto pela contratação de serviços profissionais para
+            acompanhamento, proteção e vigilância
+            <br />
+            Por que foi criado? A segurança pessoal foi criada com o objetivo
+            principal de preservar a integridade física e psicológica de um
+            indivíduo, minimizando riscos e ameaças à sua vida, liberdade e
+            privacidade. O conceito é um direito humano fundamental, consagrado
+            em documentos como a Declaração Universal dos Direitos Humanos, que
+            estabelece que todo ser humano tem direito à vida, à liberdade e à
+            segurança pessoal.
+          </p>
+        </div>
+      </section>
+
+      <section className={style.s4} id="s4">
+        <div>
+          <h2>Liberdade de expressão</h2>
+          <p>
+            A liberdade de expressão é o direito fundamental de buscar, receber
+            e difundir informações e ideias sem interferência, por qualquer
+            meio. Esse direito permite que cada pessoa se manifeste livremente,
+            expressando pensamentos, opiniões, críticas e crenças, mas também
+            implica que os indivíduos são responsáveis pelos abusos cometidos,
+            como discursos de ódio ou calúnia, que são submetidos às leis civis
+            e criminais. O direito é protegido por leis nacionais e tratados
+            internacionais, mas pode ser limitado quando entra em conflito com
+            outros direitos fundamentais ou interesses públicos.
+            <br />
+            Por que foi criado? A liberdade de expressão foi criada
+            principalmente para servir de alicerce para a democracia, permitindo
+            a participação cívica, o progresso social e o desenvolvimento
+            individual. Historicamente, essa ideia tem raízes na Grécia Antiga,
+            com os filósofos que defendiam a liberdade de opinião, e foi
+            consolidada no Iluminismo com o surgimento de constituições modernas
+            e documentos como a Primeira Emenda da Constituição dos EUA e a
+            Declaração dos Direitos do Homem e do Cidadão, que a estabeleceram
+            como um direito fundamental e inalienável.
+          </p>
+        </div>
+
+        <div>
+           <img
+              className={style.img}
+              src={foto}
+              alt="Representação"
+            />
+        </div>
+
+      </section>
+      <section className={style.s5} id="s5">
+        <div>
+          <h2>Liberdade de trabalho</h2>
+          <p>
+            Liberdade de trabalho é o direito de escolher livremente uma
+            profissão ou ofício, sem coerção, garantido pela Constituição
+            Federal do Brasil (Art. 5º, XIII) e pela legislação. Esse direito é
+            limitado apenas por qualificações exigidas por lei e pelo interesse
+            coletivo, e não permite condições análogas à escravidão. É um
+            princípio fundamental do Direito do Trabalho, que visa à dignidade
+            humana e à justiça social.
+            <br />
+            por que foi criado? A liberdade de trabalho foi estabelecida como um
+            direito humano e civil fundamental para garantir a autonomia
+            individual, combater a exploração e promover a justiça social e o
+            desenvolvimento econômico. Embora a regra seja a liberdade, o
+            exercício de certas profissões é condicionado a qualificações legais
+            para garantir a segurança e o bem-estar da sociedade.
+          </p>
+        </div>
+
+        <div>
+          <h2>Direitos Humanos</h2>
+          <p>
+            A relação entre direitos humanos e educação se manifesta de duas
+            formas principais: a educação como um direito humano fundamental e a
+            educação em direitos humanos. O primeiro é a garantia do acesso à
+            instrução para todos, enquanto o segundo se refere à educação que
+            promove o respeito à dignidade e às liberdades de todas as pessoas.
+            A educação em direitos humanos busca uma mudança cultural, focada no
+            desenvolvimento de valores éticos, tolerância e cidadania ativa.
+            <br />
+            O direito humano à educação foi criado e reconhecido
+            internacionalmente no contexto do pós-Segunda Guerra Mundial, a
+            partir de um esforço da Organização das Nações Unidas (ONU) para
+            promover a paz, reduzir desigualdades e prevenir futuras
+            atrocidades.
+          </p>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
